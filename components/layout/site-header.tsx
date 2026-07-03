672631ac61b51b9817b209697324e42d9fe9bc9f
atharvaarbat <arbatatharva130@gmail.com>
2026-07-03 14:23:22 +0530

    refactor: reorganize sidebar components and update layout structure

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { ModeToggle } from "./ui/theme-toggle"
+import { ModeToggle } from "../ui/theme-toggle"
 
 export function SiteHeader() {
   return (
